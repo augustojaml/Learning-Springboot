dbaa13467f70a81a8db2697afc91cc1857e52a3d
augustojaml <jamonteirolima@gmail.com>
2022-02-14 03:17:30 -0300

    Nelio Alves - Criando projeto com mongodb

---
+++ nelioalves-mongodb/src/main/java/com/springboot/nelioalvesmongodb/NelioalvesMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.nelioalvesmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelioalvesMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NelioalvesMongodbApplication.class, args);
+	}
+
+}
---
+++ nelioalves-mongodb/src/test/java/com/springboot/nelioalvesmongodb/NelioalvesMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.nelioalvesmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelioalvesMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
